6cf389f81df5b997acc6524842584986ef93cc46
fastestlife <hello@fastestlife.com>
2025-07-07 04:10:57 +0900

    refactor: 환경변수 기반으로 토큰 처리

--- auth.js
+++ auth.js
@@ -1,20 +1,16 @@
 // auth.js
 const { google } = require('googleapis');
-const fs = require('fs');
-const path = require('path');
-
-const TOKEN_PATH = path.join(__dirname, 'tokens.json');
 
 function getAuthenticatedClient() {
-  const credentials = JSON.parse(fs.readFileSync(TOKEN_PATH, 'utf8'));
-
   const oauth2Client = new google.auth.OAuth2(
     process.env.CLIENT_ID,
     process.env.CLIENT_SECRET,
     process.env.REDIRECT_URI
   );
 
-  oauth2Client.setCredentials(credentials); // 🔄 전체 토큰 객체 그대로 넣기
+  oauth2Client.setCredentials({
+    refresh_token: process.env.REFRESH_TOKEN
+  });
 
   return oauth2Client;
 }
